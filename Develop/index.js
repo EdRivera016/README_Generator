edfd56b855c893a41585c911a5032cc5852641de
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-05 15:36:57 -0400

    installed inquierer and started addindg packages needed

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
 
+const fs = require("fs");
+const inquirer =require("inquirer");
+const path = require("path");
+const generateMarkdown = require(".utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: "input",
+    name: "title",
+    message: "Please name your Project.",
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose and funcitonality of this project.",
+},
+{
+    type: "input",
+    name: ""
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
